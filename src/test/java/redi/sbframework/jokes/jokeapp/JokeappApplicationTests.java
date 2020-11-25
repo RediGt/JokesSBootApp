ad9461d4d15ea7f607bded54362bd5828b3b6d91
“red.i” <red.i@inbox.lv>
2020-11-25 10:34:43 +0200

    Git init

---
+++ src/test/java/redi/sbframework/jokes/jokeapp/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package redi.sbframework.jokes.jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
